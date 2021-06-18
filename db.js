eac0cf9c4179df8f4b4d26fc6882f1045e2b7814
Clement <Clementnjk@gmail.com>
2021-06-18 23:52:57 +0800

    Fixed mongo uri

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGO_URI || 'mongodb://localhost:27017/BlogSite',
+    process.env.MONGODB_URI,
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 
 )
